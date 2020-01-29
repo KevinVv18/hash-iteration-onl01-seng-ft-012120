ac90b365a819fee027180f908f1c497428e31c63
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-29 05:24:44 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,7 @@ end # add your code snippet here!
 def age_appropriate_birthday(birthday_kids)
   
 birthday_kids.each do |kids_name, age|
+  if age < 13 
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
